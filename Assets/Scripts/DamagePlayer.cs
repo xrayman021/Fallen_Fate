64e85c7b8f4cadbdb640541df7f77186bf35fbbf
Chase's Hub <chaseghorton99@gmail.com>
2022-02-05 16:09:31 -0600

    Coded DamagePlayer to inflict damage on collision

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamagePlayer : MonoBehaviour
+namespace CH
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DamagePlayer : MonoBehaviour
     {
-        
-    }
+        public int damage = 25;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnTriggerEnter(Collider other)
+        {
+            PlayerStats playerStats = other.GetComponent<PlayerStats>();
+
+            if(playerStats != null)
+            {
+                playerStats.TakeDamage(damage);
+            }
+        }
     }
 }
