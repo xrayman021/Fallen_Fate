97147af6e3fab27d538f9ecc61279d37f11d716f
Chase's Hub <chaseghorton99@gmail.com>
2022-02-05 16:05:36 -0600

    Coded PlayerStats for health functions

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+namespace CH
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerStats : MonoBehaviour
     {
-        
-    }
+        public int healthLevel = 10;
+        public int maxHealth;
+        public int currentHealth;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public HealthBar healthBar;
+
+        void Start()
+        {
+            maxHealth = SetMaxHealthFromHealthLevel();
+            currentHealth = maxHealth;
+            healthBar.SetMaxHealth(maxHealth);
+        }
+
+        private int SetMaxHealthFromHealthLevel()
+        {
+            maxHealth = healthLevel * 10;
+            return maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth = currentHealth - damage;
+            healthBar.SetCurrentHealth(currentHealth);
+        }
     }
 }
