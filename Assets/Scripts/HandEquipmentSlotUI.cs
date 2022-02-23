5138da9824fa592dbff269a8ce4838a2e0936a5f
Chase's Hub <chaseghorton99@gmail.com>
2022-02-23 12:26:53 -0600

    Coded HandEquipmentSlotUI Script

--- Assets/Scripts/HandEquipmentSlotUI.cs
+++ Assets/Scripts/HandEquipmentSlotUI.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HandEquipmentSlotUI : MonoBehaviour
+namespace CH
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HandEquipmentSlotUI : MonoBehaviour
     {
-        
-    }
+        public Image icon;
+        WeaponItem weapon;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public bool rightHandSlot01;
+        public bool rightHandSlot02;
+
+        public bool leftHandSlot01;
+        public bool leftHandSlot02;
+
+        public void AddItem(WeaponItem newWeapon)
+        {
+            weapon = newWeapon;
+            icon.sprite = weapon.itemIcon;
+            icon.enabled = true;
+            gameObject.SetActive(true);
+        }
+
+        public void ClearItem()
+        {
+            weapon = null;
+            icon.sprite = null;
+            icon.enabled = false;
+            gameObject.SetActive(false);
+        }
     }
 }
